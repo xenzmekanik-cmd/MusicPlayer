ca97472d6a2ac6a4c47cb0173e355b9e5a48a58e
xenzmekanik-cmd <xenzmekanik@gmail.com>
2026-06-18 01:57:17 +0700

    Update startup.sh

--- startup.sh
+++ startup.sh
@@ -1,31 +1,7 @@
 #!/bin/bash
 
-echo ">> FETCHING UPSTREAM..."
-git clone https://github.com/AsmSafone/MusicPlayer /MusicPlayer
+echo "Starting MusicPlayer..."
 
-echo ">> INSTALLING REQUIREMENTS..."
-cd /MusicPlayer
-pip3 install -U -r requirements.txt
+pip install -U -r requirements.txt
 
-echo ">> STARTING MUSIC PLAYER USERBOT..."
-clear
-echo "
-#     #                        
-##   ## #    #  ####  #  ####  
-# # # # #    # #      # #    # 
-#  #  # #    #  ####  # #      
-#     # #    #      # # #      
-#     # #    # #    # # #    # 
-#     #  ####   ####  #  ####  
-                               
-######                                    
-#     # #        ##   #   # ###### #####  
-#     # #       #  #   # #  #      #    # 
-######  #      #    #   #   #####  #    # 
-#       #      ######   #   #      #####  
-#       #      #    #   #   #      #   #  
-#       ###### #    #   #   ###### #    # 
-                                          
-MUSIC PLAYER USERBOT IS SUCCESSFULLY DEPLOYED!
-"
-python3 main.py
+python main.py
